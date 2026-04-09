6527b283bc9d9ca77788f95481f04f25ae8b5fc5
Téti Miklós <mikiteti@icloud.com>
2026-04-09 18:55:02 +0200

    added support for multiple allowed hosts

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-    origin: process.env.ALLOW,
+    origin: process.env.ALLOW.split(","),
     credentials: true
 }));
 // app.use(cors({
@@ -23,8 +23,8 @@ app.use(cors({
 //     },
 //     credentials: true
 // }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(
     session({
         store: new SQLiteStore({
